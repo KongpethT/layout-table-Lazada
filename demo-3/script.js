fbfa77be70986d2ff221783e263326f6d2c4ebef
kongpetht <kongpetht@hotmail.com>
2023-02-24 17:48:10 +0700

    commit

---
+++ demo-3/script.js
@@ -0,0 +1,11 @@
+let menu = {
+    home: "home",
+    contact: "contact",
+    new_media: "new & media",
+    investors: "investors"
+
+}
+document.getElementById('home').innerHTML = menu.home
+document.getElementById('contact').innerHTML = menu.contact
+document.getElementById('new_media').innerHTML = menu.new_media
+document.getElementById('investors').innerHTML = menu.investors
